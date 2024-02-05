3644945511c43e9d59f9a69d17f95cb64848300c
RITIK <ritikbansal7272@gmail.com>
2024-02-05 16:34:51 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const url= require('url')
+const PORT= porcess.env.PORT || 8000
+const server= http.createServer((req,res)=>
+{
+    const URL = url.parse(req.url)
+    ptname =URL.pathname;
+    console.log(ptname)
+    switch(ptname)
+    {
+         case '/':
+         
+            res.end("<h1> HOME PAGE </h1>")
+            break;
+        
+        case '/about':
+         
+            res.end("<h1> ABOUT PAGE </h1>")
+            break;
+        default :
+            res.end("<h1> invalid url </h1>")
+
+        
+    }
+}
+)
+server.listen(PORT,console.log("server is started"));
